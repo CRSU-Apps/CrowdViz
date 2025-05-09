bb9fbfc14ec88eb35fb4531626cb6358b029eeb5
JanionNevill <janion.nevill@leicester.ac.uk>
2025-05-09 09:26:57 +0100

    Updated imports

--- R/PopViz.R
+++ R/PopViz.R
@@ -14,6 +14,8 @@ library(ggplot2)
 #'
 #' @return ggplot2 plot object
 #' @export
+#' 
+#' @importFrom ggplot2 ggplot aes geom_tile scale_fill_continuous geom_line xlim ylim annotate theme_void theme ggtitle element_text
 #'
 #' @examples
 #' PopViz(NoPeople = 50,
