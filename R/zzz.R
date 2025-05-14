9370fe1136733ab940d8a3051f7934b244cfac9e
JanionNevill <janion.nevill@leicester.ac.uk>
2025-05-14 12:35:03 +0100

    Minor tidy-up

--- R/zzz.R
+++ R/zzz.R
@@ -2,19 +2,19 @@
 #' 
 NULL
 
-person_solid <- file.path("inst","person-solid.svg")
-person_narrow <- file.path("inst","person-narrow.svg")
-person_super_narrow <- file.path("inst","person-super-narrow.svg")
-person_solid_dress <- file.path("inst","person-dress-solid.svg")
-person_narrow_dress <- file.path("inst","person-dress-narrow.svg")
-person_super_narrow_dress <- file.path("inst","person-dress-super-narrow.svg")
+person_solid <- file.path("inst", "person-solid.svg")
+person_narrow <- file.path("inst", "person-narrow.svg")
+person_super_narrow <- file.path("inst", "person-super-narrow.svg")
+person_dress_solid_ <- file.path("inst", "person-dress-solid.svg")
+person_dress_narrow <- file.path("inst", "person-dress-narrow.svg")
+person_dress_super_narrow <- file.path("inst", "person-dress-super-narrow.svg")
 
 .onLoad <- function (libname, pkgname) {
   ns <- topenv()
   ns$person_solid <- system.file("person-solid.svg", package = "PopViz")
   ns$person_narrow <- system.file("person-narrow.svg", package = "PopViz")
   ns$person_super_narrow <- system.file("person-super-narrow.svg", package = "PopViz")
-  ns$person_solid_dress <- system.file("person-dress-solid.svg", package = "PopViz")
-  ns$person_narrow_dress <- system.file("person-dress-narrow.svg", package = "PopViz")
-  ns$person_super_narrow_dress <- system.file("person-dress-super-narrow.svg", package = "PopViz")
+  ns$person_dress_solid <- system.file("person-dress-solid.svg", package = "PopViz")
+  ns$person_dress_narrow <- system.file("person-dress-narrow.svg", package = "PopViz")
+  ns$person_dress_super_narrow <- system.file("person-dress-super-narrow.svg", package = "PopViz")
 }
